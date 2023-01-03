fc769c752d83fe4548177c84d70c5899bf83e571
정민철 <tlffur20@gmail.com>
2023-01-03 19:44:41 +0900

    .

--- src/components/Button2.jsx
+++ src/components/Button2.jsx
@@ -2,5 +2,5 @@ import React from 'react';
 import styles from './Button2.module.css';
 
 export default function Button2() {
-  return <button className={styles.button}>Button2</button>;
+  return <button className={styles.button}>Button2-장난아님!</button>;
 }
